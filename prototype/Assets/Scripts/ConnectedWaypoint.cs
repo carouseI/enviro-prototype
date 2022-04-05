7365ec201813871f5bbdfc89d93f8f4a1f1f412d
headlesscrown <mdh8533@nyu.edu>
2022-04-05 16:05:37 -0400

    new waypoint script

--- prototype/Assets/Scripts/ConnectedWaypoint.cs
+++ prototype/Assets/Scripts/ConnectedWaypoint.cs
@@ -1,18 +1,40 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
-public class ConnectedWaypoint : MonoBehaviour
+namespace Assets.Code
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ConnectedWaypoint : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        protected float _connectivityRadius = 50f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        List<ConnectedWaypoint> _connections;
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+            GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint"); //get all waypoint objects in scene
+
+            _connections = new List<ConnectedWaypoint>(); //create list of waypoint refs
+
+            for(int i = 0; i < allWaypoints.Length; i++)
+            {
+                ConnectedWaypoint nextWaypoint = allWaypoints[i].GetComponent<ConnectedWaypoint>(); //check for connected waypoint
+
+                if(nextWaypoint != null) //if found + not null
+                {
+                    if(Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= connecti)
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
---
+++ prototype/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawRadius = 1.0F; //set gizmo radius
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; //set gizmo colour
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius); //generate sphere
+    }
+}
